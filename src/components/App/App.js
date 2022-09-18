3e308a99876cc2d97c46854f022e56538c9d1201
Kyle Chivers <chiverskyle@gmail.com>
2022-09-18 13:02:28 -0700

    created class based component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
-function App() {
-    return <h1>This is my functional component!</h1>
+class App extends React.Component {
+    render() {
+        return <h1>This is my class based component!</h1>;
+    }
 }
 
 export default App
