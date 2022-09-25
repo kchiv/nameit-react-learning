4f0db255f927886bcf338309e1ad357ab123ae4c
Kyle Chivers <chiverskyle@gmail.com>
2022-09-25 15:06:05 -0700

    ternary operator to assign class based on state
    Uses a ternary operator to set different CSS class based upon user interaction with the SearchBox component. This uses template literals in order to inject the ternary operator into the middle of the class name string.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,11 @@ const Header = (props) => {
         <div className='head-container'>
             <img 
                 src='https://user-images.githubusercontent.com/26179770/106359099-693e3380-6336-11eb-8069-9e36f25de5ca.png' 
-                className='head-image'
+                className={`head-image ${
+                    props.headerExpanded 
+                    ? 'head-image-expanded' 
+                    : 'head-image-contracted'
+                }`}
                 alt='headerImage'
             />
             <h1 className='head-text'>{props.headTitle}</h1>
