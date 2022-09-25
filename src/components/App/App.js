d93c70d01cc06901d0568ed38fa6d1ec03d95bf1
Kyle Chivers <chiverskyle@gmail.com>
2022-09-25 13:31:03 -0700

    remove h3

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,13 +10,13 @@ class App extends React.Component {
 
     state = {
         headerText: 'This is a cool app!',
-    }
+    };
 
     render() {
         return (
             <div>
                 <Header />
-                <h3>{this.state.headerText}</h3>
+                
             </div>
         )
     }
