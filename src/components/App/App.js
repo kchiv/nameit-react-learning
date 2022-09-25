b07aec77862442911a7d4a4961130cdacb816db0
Kyle Chivers <chiverskyle@gmail.com>
2022-09-25 13:11:55 -0700

    created header text with state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,10 +3,16 @@ import Header from '../Header/Header';
 import './App.css';
 
 class App extends React.Component {
+
+    state = {
+        headerText: 'This is a cool app!',
+    }
+
     render() {
         return (
             <div>
                 <Header />
+                <h3>{this.state.headerText}</h3>
             </div>
         )
     }
