5205572c27d59b1f5bc0b1505af73902d2d5a16b
Kyle Chivers <chiverskyle@gmail.com>
2022-10-16 09:40:17 -0700

    new results component added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../Header/Header';
+import ResultsContainer from '../ResultsContainer/ResultsContainer';
 import SearchBox from '../SearchBox/SearchBox';
 import './App.css';
 
@@ -28,6 +29,7 @@ class App extends React.Component {
                     headerExpanded={this.state.headerExpanded}
                     headTitle={this.state.headerText} />
                 <SearchBox onInputChange={this.handleInputChange} />
+                <ResultsContainer />
             </div>
         );
     }
---
+++ src/components/ResultsContainer/ResultsContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ResultsContainer = () => {
+    return (
+        <div>
+            <p>These are the results!</p>
+        </div>
+    );
+};
+
+export default ResultsContainer;
