ee0ac159643de23e961e72c083dc777fb31247ec
Kyle Chivers <chiverskyle@gmail.com>
2022-10-16 13:26:15 -0700

    added link to namecheap

--- src/components/NameCard/NameCard.js
+++ src/components/NameCard/NameCard.js
@@ -1,11 +1,15 @@
 import React from "react";
 import './NameCard.css';
 
+const nameCheapUrl = "https://www.namecheap.com/domains/registration/results/?domain=";
+
 const NameCard = ({ suggestedName }) => {
     return (
-        <div className="result-name-card">
-            <p className="result-name">{suggestedName}</p>
-        </div>
+        <a className="card-link" href={`${nameCheapUrl}${suggestedName}`}>
+            <div className="result-name-card">
+                <p className="result-name">{suggestedName}</p>
+            </div>
+        </a>
     )
 }
 
