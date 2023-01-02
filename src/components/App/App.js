7b49e1c7240d3542be17766110b55a6c0007a51b
Kyle Chivers <chiverskyle@gmail.com>
2023-01-02 11:53:18 -0800

    converted to class to function
    Converted the class based component to a function based component that can use state.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from '../Header/Header';
 import ResultsContainer from '../ResultsContainer/ResultsContainer';
 import SearchBox from '../SearchBox/SearchBox';
@@ -6,39 +6,25 @@ import './App.css';
 
 const name = require('@rstacruz/startup-name-generator');
 
-class App extends React.Component {
+const App = () => {
+    const [headerText, setHeaderText] = useState('Name It!');
+    const [headerExpanded, setHeaderExpanded] = useState(true);
+    const [suggestedNames, setSuggestedNames] = useState([]);
 
-    // Notes: Changing state directly does not trigger a re-render
-    // Example this.state.headerText = 'new state' does not trigger a re-render
-    // To change state we have to use setState
-
-    constructor() {
-        super();
-        this.state = {
-            headerText: 'Name It!',
-            headerExpanded: true,
-            suggestedNames: []
-        };
+    const handleInputChange = (inputText) => {
+        setHeaderExpanded(!inputText);
+        setSuggestedNames(inputText ? name(inputText) : []);
     }
 
-    handleInputChange = (inputText) => {
-        this.setState({
-            headerExpanded: !inputText,
-            suggestedNames: inputText ? name(inputText) : [],
-        });
-    }
-
-    render() {
-        return (
-            <div>
-                <Header 
-                    headerExpanded={this.state.headerExpanded}
-                    headTitle={this.state.headerText} />
-                <SearchBox onInputChange={this.handleInputChange} />
-                <ResultsContainer suggestedNames={this.state.suggestedNames} />
-            </div>
-        );
-    }
+    return (
+        <div>
+            <Header 
+                headerExpanded={headerExpanded}
+                headTitle={headerText} />
+            <SearchBox onInputChange={handleInputChange} />
+            <ResultsContainer suggestedNames={suggestedNames} />
+        </div>
+    );
 }
 
 export default App;
